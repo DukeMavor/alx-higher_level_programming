59e1bcab574fafa9c809d746b2508dc2eed2bd35
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-23 12:39:41 +0100

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import traceback as tb
+
+
 def safe_function(fct, *args):
-    import sys
     try:
-        return(fct(*args))
-    except:
-        sys.stderr.write("Exception: {}\n".format(sys.exc_info()[1]))
-        return(None)
+        result = fct(*args)
+    except Exception as e:
+        tb.print_exception(Exception, Exception(e), None)
+        result = None
+    return result
