62da4cd4b3606c965804017ed3cc9ea5fef602ca
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-23 12:23:39 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        return False
-    else:
         return True
+    except (ValueError, TypeError):
+        return False
