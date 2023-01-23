221792f14a3c5ecf51b14603d33358af8046d3bd
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-23 12:22:41 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    printed = 0
-    if(my_list):
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end='')
-                printed += 1
-            except IndexError:
-                print()
-                return(printed)
-    print()
-    return(printed)
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except:
+        return False
+    else:
+        return True
