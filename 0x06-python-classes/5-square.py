b3290fae718807cdcd60a75fe37c2c7be17fb41f
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-24 08:29:08 +0100

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class to represent a square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.__size is 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
