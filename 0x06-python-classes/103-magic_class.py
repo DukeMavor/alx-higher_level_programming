c6f74ded736cce847c0dcf561fa1fd04d7946307
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-24 13:20:01 +0100

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+""" Module providing class 'MagicClass' to replicate bytecode for Holberton """
 import math
 
 
-class MagicClass:
+class MagicClass():
+    """ Definition of a class to replicate the afformentioned bytecode """
     def __init__(self, radius=0):
-        """Initialize circle with default radius=0
-
-        __init__ method initializes radius and test if numeric"""
+        """ Instantiate a MagicClass object to represent a circle """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """Calculate area of circle based on radius"""
-        return self.__radius ** 2 * math.pi
+        """ Compute the area of a circle """
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """Calculate circumference with given radius"""
+        """ Compute the circumference of a circle """
         return 2 * math.pi * self.__radius
