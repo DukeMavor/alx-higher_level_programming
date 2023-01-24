b0448f35eda449e1db6a6a66377b2a799c29d8ab
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-24 13:15:10 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
 
 
 class Square:
-    """Class defining square"""
+    """class Square that defines a square"""
+    __size = None
+
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """Instantiation with optional size"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            raise TypeError("size must be an integer")
