4228784ac3b0edae24d1fa613d23dfdf6c1e8c57
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-24 13:18:04 +0100

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,64 +1,57 @@
 #!/usr/bin/python3
+""" Module providing a 'Node' class for a singly-linked list
+"""
 
 
-class Node:
-    """Class to represent node of singly linked list"""
+class Node():
+    """ Definition of a singly-linked list node
+    """
     def __init__(self, data, next_node=None):
-        self.data = data
-        self.next_node = next_node
+        """ Instantiate a node
+        """
+        self.data, self.next_node = data, next_node
 
     @property
     def data(self):
+        """ Get the data stored in a node
+        """
         return self.__data
 
     @data.setter
-    def data(self, value):
-        if type(value) is not int:
-            raise TypeError('data must be an integer')
-        self.__data = value
+    def data(self, data):
+        """ Set the data stored in a node
+        """
+        if not isinstance(data, int):
+            raise TypeError("data must be an integer")
+        self.__data = data
 
     @property
     def next_node(self):
+        """ Get the next node
+        """
         return self.__next_node
 
     @next_node.setter
-    def next_node(self, value):
-        if value is not None and type(value) is not Node:
-            raise TypeError('next_node must be a Node object')
-        self.__next_node = value
+    def next_node(self, next_node):
+        """ Set the next node
+        """
+        if next_node is not None and not isinstance(next_node, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = next_node
 
 
-class SinglyLinkedList:
-    """Class to store singly linked list of Node objects"""
+class SinglyLinkedList():
+    """ Definition of a singly-linked list
+    """
     def __init__(self):
+        """ Instantiate a singly-linked list
+        """
         self.__head = None
 
-    def sorted_insert(self, value):
-        node = Node(value)
-        if self.__head is None:
-            self.__head = node
-        else:
-            head = self.__head
-            if value < head.data:
-                node.next_node = head
-                self.__head = node
-                return
-            while head.next_node is not None:
-                if value < head.next_node.data:
-                    node.next_node = head.next_node
-                    head.next_node = node
-                    break
-                head = head.next_node
-            head.next_node = node
-
     def __str__(self):
-        head = self.__head
-        ret = ""
-        if head is None:
-            return ret
-        while head.next_node is not None:
-            ret += format(head.data, 'd')
-            ret += '\n'
-            head = head.next_node
-        ret += format(head.data, 'd')
-        return ret
+        """ Generate a visual representation of a list
+        """
+
+    def sorted_insert(self, value):
+        """ Inset a Node into a list sorted in ascending order
+        """
