8a358f87c11bbd12d50ab1ee055f8c5ed83c55cf
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-24 08:27:34 +0100

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class defining square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
