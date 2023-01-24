2b8786f53a592ec70a53b710b5a7af6283d8abfe
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-24 13:15:40 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
 
 
 class Square:
-    """Class to represent a square"""
+    """class Square that defines a square"""
+    __size = None
+
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """Instation with optional size"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("Size must be >= 0")
         else:
-            self.__size = size
+            raise TypeError("size must be an integer")
 
     def area(self):
-        return (self.__size ** 2)
+        """Public instance method area"""
+        return self.__size * self.__size
