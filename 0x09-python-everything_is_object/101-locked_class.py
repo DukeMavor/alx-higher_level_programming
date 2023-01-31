17f49fb7df5f3c71cc8ec42bbe4f54167f3a0c9d
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-31 12:55:53 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-
 class LockedClass:
-    __slots__ = ['first_name']
+    def __setattr__(self, attr, value):
+        if attr != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
+        self.__dict__.update({attr: value})
