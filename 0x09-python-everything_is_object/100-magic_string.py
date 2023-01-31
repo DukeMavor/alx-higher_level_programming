f4444b664ee88a9afe8ff45147cf4900c65e6990
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-01-31 12:59:33 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append('BestSchool')
-    return ', '.join(l)
+def magic_string():
+    magic_string.i = getattr(magic_string, 'i', -1) + 1
+    return 'BestSchool' + ', BestSchool'*magic_string.i
