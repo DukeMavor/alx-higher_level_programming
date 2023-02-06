5e1ba7043e5768772614ffc469397dc8bca11d07
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-02-06 14:57:40 +0100

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
+"""adds new attribute if possible"""
 
 
 def add_attribute(obj, attr, value):
-    """Defines function to check whether object supports dynamic assigment.
-
-    Args:
-        obj: object to check/assign to
-        attr (str): name of attribute to assign to `obj`
-        value: value to be associated with `name`
-
-    Raises:
-        TypeError: If `obj` does not support dynamic assignment
-    """
-    if not isinstance(attr, str) or \
-       hasattr(obj, attr) or not\
-       hasattr(obj, "__setattr__"):
+    """adds new attribute"""
+    if('__slots__' in dir(obj) or '__dict__' not in dir(obj) or
+       hasattr(obj, attr)):
         raise TypeError("can't add new attribute")
     setattr(obj, attr, value)
