deae34c759e28ddb8d680fa5ae4a9fb53b5201c0
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-02-07 12:48:49 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-"""Program to save strings from command line arguments to file called
-`add_item.json`. File contains a json serialized list of all strings
-entered as arguments to the program.
+"""loads adds saves json to file"""
+import json
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-Example:
-    $ ./9-add_item.py Holberton School
-    $ cat add_item.json ; echo
-    ["Holberton", "School"]
-    $ ./9-add_item.py Python 89 C
-    $ cat add_item.json ; echo
-    ["Holberton", "School", "Python", "89", "C"]
-"""
+try:
+    jsonList = load_from_json_file("add_item.json")
+except:
+    jsonList = []
 
-if __name__ == "__main__":
-    import sys
-    import json
-    save_to_json_file = \
-        __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
-
-    filename = "add_item.json"
-    with open(filename, 'a+') as f:  # Create add_item.json, if necessary
-        if f.tell() == 0:
-            json.dump([], f)
-    file_data = load_from_json_file("add_item.json")
-    if len(sys.argv) > 1:
-        file_data.extend(sys.argv[1:])
-    save_to_json_file(file_data, filename)
+for i in sys.argv[1:]:
+    jsonList.append(i)
+save_to_json_file(jsonList, "add_item.json")
