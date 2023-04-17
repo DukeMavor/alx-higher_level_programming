6c1450afabfe19f237fab4ae6d3b914eb161ed0a
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-04-17 13:57:17 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""SQL injection safe query to get rows of states where name matches
-an argument.
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         database=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states "
-                "WHERE name = %s "
-                "COLLATE 'latin1_general_cs' "
-                "ORDER BY id ASC", (sys.argv[4],))
-    for row in cur.fetchall():
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     cur.close()
     db.close()
