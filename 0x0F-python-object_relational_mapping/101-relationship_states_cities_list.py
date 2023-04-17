8bc8aae3512e167d5f9060843c93d9ee810713fc
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-04-17 14:29:34 +0100

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Script to create State `California` with City `San Francisco` within
-database hbtn_0e_100_usa.
+""" prints the State object with the name passed as argument from the database
 """
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
-    import sys
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from relationship_city import City
-    from relationship_state import State, Base
-
-    engine = create_engine('mysql+mysqldb://'
-                           '{}:{}@localhost/{}'
-                           .format(sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3]))
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    ca = State(name='California')
-    sf = City(name='San Francisco')
-    ca.cities = [sf]
-    session.add(ca)
-    session.commit()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
