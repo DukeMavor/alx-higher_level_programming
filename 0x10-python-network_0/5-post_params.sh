95a4a1c3bbcc709b4b2707c804f554c4d0d2de92
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-04-27 12:36:34 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Post some data to a web server
-curl -sd 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
