ab8ae3f455cc34c03af8d656dfc85b396a8cbeec
Victor Awunudo <113598114+DukeMavor@users.noreply.github.com>
2023-04-27 12:37:58 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of integers.
+    """Finds a peak in list_of_integers"""
 
-    Example:
-        [1, 2, 4, 6, 3] -> 6
-        [4, 2, 1, 2, 3, 1] -> 3 # but 4 is also a peak
-
-    Returns: integer that is the peak.
-    """
-    l = len(list_of_integers)
-    n = l // 2
-    v = 0
-    while v < l:
-        v += 1
-        if n == 0:
-            gtl = True
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    lo = 0
+    hi = len(list_of_integers) - 1
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            lo = mid + 1
         else:
-            gtl = False
-        if n == l - 1:
-            gtr = True
-        else:
-            gtr = False
-        if n > 0:
-            if list_of_integers[n - 1] < list_of_integers[n]:
-                gtl = True
-        if n < l - 1:
-            if list_of_integers[n] > list_of_integers[n + 1]:
-                gtr = True
-        if gtl and gtr:
-            return list_of_integers[n]
-        if not gtr:
-            n += (l - n) // 2
-        elif not gtl:
-            n -= n // 2
+            hi = mid
+    return list_of_integers[lo]
